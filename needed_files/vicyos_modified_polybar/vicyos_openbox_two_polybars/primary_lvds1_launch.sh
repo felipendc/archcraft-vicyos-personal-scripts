c60722b98166c424f4b72fefeb094a1771765a6d
felipendc <felipendc10@gmail.com>
2020-11-18 11:15:23 -0300

    Vicyos Rolling Update: (qua 18 nov 2020 11:15:23 -03)

--- needed_files/vicyos_modified_polybar/vicyos_openbox_two_polybars/primary_lvds1_launch.sh
+++ needed_files/vicyos_modified_polybar/vicyos_openbox_two_polybars/primary_lvds1_launch.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # Terminate already running bar instances
-killall -q polybar
+# killall -q polybar
 
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+# # Wait until the processes have been shut down
+# while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch the bar
 polybar main1 -c "$HOME"/.config/polybar/vicyos_openbox_two_polybars/config_primary.ini &
--- needed_files/vicyos_modified_polybar/vicyos_two_polybars.sh
+++ needed_files/vicyos_modified_polybar/vicyos_two_polybars.sh
@@ -3,6 +3,11 @@
 # Launch the bar
 STYLE="vicyos_openbox_two_polybars"
 
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
 # Launch Polybar for primary monitor LVDS1
 bash "$HOME"/.config/polybar/"$STYLE"/primary_lvds1_launch.sh
 
