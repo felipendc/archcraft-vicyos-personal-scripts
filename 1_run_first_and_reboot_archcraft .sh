9132b9dceb5bf82368d74afaf9e7576edf7754c7
felipendc <felipendc10@gmail.com>
2020-11-19 15:44:32 -0300

    Vicyos Rolling Update: (qui 19 nov 2020 15:44:32 -03)

--- 1_run_first_and_reboot_archcraft .sh
+++ 1_run_first_and_reboot_archcraft .sh
@@ -10,7 +10,92 @@ source ./needed_files/vicyos_banner/vicyos_setup_banner.sh
 # My Github             :   github.com/felipendc               #
 # Important Note        :   I use Arch! btw... Hahaha!         #
 ################################################################
+install_trizen(){
+# Unpacking file
+rm -Rvf trizen
+tar -xvf $trizen_file
 
+# Compile trizen, install it, and clean up the temp files
+cd trizen
+makepkg -s
+sudo pacman -U trizen*.zst --noconfirm
+cd ../
+rm -Rvf trizen
+sudo rm -Rv $trizen_file
+}
+
+snapshot(){
+# It waits 5 secs and then it downloads the trizen snapshot (PKGBUILD file) 
+# and self stop after the download is finished!
+sleep 5
+curl https://aur.archlinux.org/cgit/aur.git/snapshot/trizen.tar.gz --output trizen.tar.gz
+}
+
+trizen(){
+trizen_file="trizen.tar.gz" 
+
+counter=0
+seconds_is=""
+
+# Wait until trizen snapshot is downloaded and trigger the trizen installation function (install_trizen)
+if  pacman -Qi trizen &> /dev/null; then
+	echo "Trizen is already installed." 
+	exit
+else
+	if [ -e "$trizen_file" ]; then
+
+		# Clean up to avoid any conflicts
+		sudo rm -Rv $trizen_file
+	
+		# It waits for the download to finished!
+		snapshot &
+	else
+		snapshot &
+	fi
+
+	#If the trizen file don't exist then, download it
+	if [ ! -e "$trizen_file" ]; then
+
+		# Loop through the below commands until trizen_file is downloaded
+		# Or wait 120 seconds (2 minutes) and exit the entire script
+		until [ -e "$trizen_file" ] 
+		do
+			# For each second add +1 to the counter variable
+			counter=$((counter+1))
+
+			# Verify if the number in "counter" is plural or singular and add it to the "seconds_is" variable
+			if [ $counter == 1 ]; then
+				seconds_is=" second"
+			else
+				seconds_is=" seconds"
+			fi
+
+			# Wait 1 second
+			sleep 1
+			if [ -e "$trizen_file" ]; then
+				echo "Congrats!!! $trizen_file file was downloaded successfully!"
+
+			elif [ "$counter" == 121 ]; then
+				clear
+				echo ""
+				echo "Download failed! $trizen_file file wasn't downloaded successfully!" 
+				echo "Time is out! 120 seconds (2 minutes) went by." 
+				echo "Please, check if your internet speed is slow or your connection is working and try again." 
+				echo ""
+				exit
+			else
+				echo "Please wait... $trizen_file is being downloaded! $counter$seconds_is"
+			fi
+		done
+	fi
+fi
+
+# Wait for each specified process to complete and return its termination status
+wait -n
+
+# Call install_trizen function
+install_trizen
+}
 
 update(){
 # Sync and refresh pacman database and update the all of the packages
@@ -72,6 +157,10 @@ trizen -S python --needed --noconfirm
 trizen -S google-chrome --needed --noconfirm
 trizen -S visual-studio-code-bin --needed --noconfirm
 trizen -S android-studio --needed --noconfirm
+trizen -S bluetooth-autoconnect --needed --noconfirm
+trizen -S bluez --needed --noconfirm
+trizen -S bluez-utils --needed --noconfirm
+trizen -S bluez-utils-compat --needed --noconfirm
 }
 
 reboot_os(){
@@ -81,5 +170,6 @@ sudo reboot
 
 loading_banner
 update
+trizen
 personal_pkgs
 reboot_os
--- 2_vicyos_archcraft-_setup.sh
+++ 2_vicyos_archcraft-_setup.sh
@@ -44,94 +44,54 @@ else
 fi
 }
 
-install_trizen(){
-# Unpacking file
-rm -Rvf trizen
-tar -xvf $trizen_file
+vicyos_polybar_desktop(){
+original_polybar="$HOME/.config/polybar/vicyos_two_polybars.sh"
+backup_folder="$HOME/vicyos_backups"
 
-# Compile trizen, install it, and clean up the temp files
-cd trizen
-makepkg -s
-sudo pacman -U trizen*.zst --noconfirm
-cd ../
-rm -Rvf trizen
-sudo rm -Rv $trizen_file
-}
+# Check if vicyos Two Polybars are already installed
+if [ -e "$original_polybar" ]; then
 
-snapshot(){
-# It waits 5 secs and then it downloads the trizen snapshot (PKGBUILD file) 
-# and self stop after the download is finished!
-sleep 5
-curl https://aur.archlinux.org/cgit/aur.git/snapshot/trizen.tar.gz --output trizen.tar.gz
-}
-
-trizen(){
-trizen_file="trizen.tar.gz" 
-
-counter=0
-seconds_is=""
-
-# Wait until trizen snapshot is downloaded and trigger the trizen installation function (install_trizen)
-if  pacman -Qi trizen &> /dev/null; then
-	echo "Trizen is already installed." 
-	exit
+	echo "Vicyos Two Polybars are already installed"
 else
-	if [ -e "$trizen_file" ]; then
-
-		# Clean up to avoid any conflicts
-		sudo rm -Rv $trizen_file
-	
-		# It waits for the download to finished!
-		snapshot &
+# Make a backup of the polybar folder to "~/vicyos_backups"
+	zip polybar_backup.zip -r ~/.config/polybar
+	if [ -d "$backup_folder" ]; then
+		rm -R $backup_folder
+		mkdir $backup_folder
 	else
-		snapshot &
+		mkdir $backup_folder
 	fi
+	mv polybar_backup.zip $backup_folder
 
-	#If the trizen file don't exist then, download it
-	if [ ! -e "$trizen_file" ]; then
+	# Make a backup of the autostart file to "~/vicyos_backups"
+	# Set the right polybar ".sh" file to autostart
+	cp -r ~/.config/openbox/autostart $backup_folder
 
-		# Loop through the below commands until trizen_file is downloaded
-		# Or wait 120 seconds (2 minutes) and exit the entire script
-		until [ -e "$trizen_file" ] 
-		do
-			# For each second add +1 to the counter variable
-			counter=$((counter+1))
+	# Remove the polybar folder to avoid any conflicts
+	# Copy the vicyos modified polybar files to ~/.config/polybar
+	rm -R ~/.config/polybar
+	cp -r needed_files/vicyos_modified_polybar ~/.config/polybar
+	
+	# Delete "autostart" file in "~/.config/openbox/" to avoid any conflicts
+	# Copy a modified "autostart" file to "~/.config/openbox/"
+	rm -Rvf $HOME/.config/openbox/autostart
+	mv $HOME/.config/polybar/autostart $HOME/.config/openbox
 
-			# Verify if the number in "counter" is plural or singular and add it to the "seconds_is" variable
-			if [ $counter == 1 ]; then
-				seconds_is=" second"
-			else
-				seconds_is=" seconds"
-			fi
-
-			# Wait 1 second
-			sleep 1
-			if [ -e "$trizen_file" ]; then
-				echo "Congrats!!! $trizen_file file was downloaded successfully!"
-
-			elif [ "$counter" == 121 ]; then
-				clear
-				echo ""
-				echo "Download failed! $trizen_file file wasn't downloaded successfully!" 
-				echo "Time is out! 120 seconds (2 minutes) went by." 
-				echo "Please, check if your internet speed is slow or your connection is working and try again." 
-				echo ""
-				exit
-			else
-				echo "Please wait... $trizen_file is being downloaded! $counter$seconds_is"
-			fi
-		done
-	fi
-fi
-
-# Wait for each specified process to complete and return its termination status
-wait -n
-
-# Call install_trizen function
-install_trizen
+	# Make some polybar files executable 
+	chmod +x ~/.config/polybar/default_launch.sh
+	chmod +x ~/.config/polybar/vicyos_two_polybars.sh
+	chmod +x ~/.config/polybar/wave/launch.sh
+	chmod +x ~/.config/polybar/spark/launch.sh
+	chmod +x ~/.config/polybar/manhattan/launch.sh
+	chmod +x ~/.config/polybar/grid/launch.sh
+	chmod +x ~/.config/polybar/forest/launch.sh
+	chmod +x ~/.config/polybar/default/launch.sh
+	chmod +x ~/.config/polybar/beach/launch.sh	
+	chmod +x ~/.config/polybar/vicyos_openbox_two_polybars/secondary_hdmi_launch.sh
+	chmod +x ~/.config/polybar/vicyos_openbox_two_polybars/primary_lvds1_launch.sh
 }
 
-vicyos_polybar_p1(){
+vicyos_polybar_laptop(){
 original_polybar="$HOME/.config/polybar/vicyos_two_polybars.sh"
 backup_folder="$HOME/vicyos_backups"
 
@@ -205,7 +165,7 @@ fi
 loading_banner
 reset_color
 add_vicyos_repo 
-trizen
-vicyos_polybar_p1
+# TODO // vicyos_polybar_desktop 
+vicyos_polybar_laptop
 vicyos_zsh
 update 
